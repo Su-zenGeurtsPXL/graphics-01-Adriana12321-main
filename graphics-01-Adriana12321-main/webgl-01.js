eacedbdffc3d37126f96980361a2f9c9341586f9
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-05-26 21:53:58 +0200

    Cleanup/OOP BufferGeometry

--- graphics-01-Adriana12321-main/webgl-01.js
+++ graphics-01-Adriana12321-main/webgl-01.js
@@ -30,39 +30,6 @@ fragColor = vec4( v_color.rgb, 1.0 ); // only assign the fragment color (NEW)
 }
 `;
 
-function useBuffer(gl, buffer, itemSize, location = 0, normalize = false) {
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.enableVertexAttribArray(location);
-  gl.vertexAttribPointer(location, itemSize, gl.FLOAT, normalize, 0, 0);
-}
-
-function createBuffer(gl, arrayData) {
-  // Create a buffer.
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-  // Upload Geometry data
-  gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(arrayData), gl.STATIC_DRAW);
-
-  return buffer;
-}
-
-function createIndex(gl, indices) {
-  // create the index buffer
-  const indexBuffer = gl.createBuffer();
-
-  // make this buffer the current 'ELEMENT_ARRAY_BUFFER'
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  gl.bufferData(
-    gl.ELEMENT_ARRAY_BUFFER,
-    Uint16Array.from(indices),
-    gl.STATIC_DRAW
-  );
-
-  return indexBuffer;
-}
-
 function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
@@ -81,31 +48,28 @@ function main() {
   }
   document.body.appendChild(canvas);
 
-  // Triangle Geometry
-  const triangle = createBuffer(gl, [0.5, 1, 0, 0, 0, 0, 1, 0.3, 0]);
+  // Triangle Geometry (NEW!)
+  const triangleGeometry = new BufferGeometry();
+  triangleGeometry.setAttribute("position", {
+    array: [0.5, 1, 0, 0, 0, 0, 1, 0.3, 0],
+    itemSize: 3,
+  });
+  triangleGeometry.setAttribute("color", {
+    array: [1, 0, 0, 1.0, 0, 1, 0, 1.0, 0, 0, 1, 1.0],
+    itemSize: 4,
+  });
 
-  // triangle color rgba
-  const triangleColors = createBuffer(
-    gl,
-    [1, 0, 0, 1.0, 0, 1, 0, 1.0, 0, 0, 1, 1.0]
-  );
-
-  // Set Geometry - Quad
-  const quad = createBuffer(
-    gl,
-    [
-      -0.1, -0.5, 0, -0.1, 0.5, 0, -0.8, -0.5, 0, -0.8, -0.5, 0, -0.1, 0.5, 0,
-      -0.8, 0.5, 0,
-    ]
-  );
-
-  // Indexed Geometry - Quad
-  const quadIndexed = createBuffer(
-    gl,
-    [-0.1, -0.5, 0, -0.1, 0.5, 0, -0.8, -0.5, 0, -0.8, 0.5, 0]
-  );
-  // Indexed Geometry - Quad
-  const indices = createIndex(gl, [
+  // Indexed Geometry – Quad (NEW!)
+  const quadIndexedGeometry = new BufferGeometry();
+  quadIndexedGeometry.setAttribute("position", {
+    array: [-0.1, -0.5, 0, -0.1, 0.5, 0, -0.8, -0.5, 0, -0.8, 0.5, 0],
+    itemSize: 3,
+  });
+  quadIndexedGeometry.setAttribute("color", {
+    array: [1, 0, 0, 1.0, 0, 1, 0, 1.0, 0, 0, 1, 1.0, 1, 1, 1, 1.0],
+    itemSize: 4,
+  });
+  quadIndexedGeometry.setIndex([
     0,
     1,
     2, // first triangle
@@ -113,12 +77,6 @@ function main() {
     1,
     3, // second triangle
   ]);
-
-  // quad color rgba
-  const quadColors = createBuffer(
-    gl,
-    [1, 0, 0, 1.0, 0, 1, 0, 1.0, 0, 0, 1, 1.0, 1, 1, 1, 1.0]
-  );
 
   // setup GLSL program
   const program = createProgram(gl, [vs, fs]);
@@ -172,16 +130,8 @@ function main() {
       new Float32Array([controls.color.r, controls.color.g, controls.color.b])
     ); // Set the color.
 
-    // triangle with color
-    useBuffer(gl, triangle, 3, 0);
-    useBuffer(gl, triangleColors, 4, 1); // (NEW)
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-    // quad with color
-    useBuffer(gl, quadIndexed, 3, 0);
-    useBuffer(gl, quadColors, 4, 1); // (NEW)
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indices);
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+    triangleGeometry.draw(gl); //( NEW!)
+    quadIndexedGeometry.draw(gl); //( NEW!)
   }
 
   // some settings that can be changed
